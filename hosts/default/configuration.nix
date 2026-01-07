05f280c3e6300c7eed18e1ff17d7eadffd36ed0c
wlog106 <xuanhao8765@gmail.com>
2026-01-07 20:40:03 +0800

    new: add boot system module

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -7,16 +7,10 @@
   imports =
     [ 
       ./hardware-configuration.nix
+      ../../modules/system/boot.nix
       ../../modules/system/timezone-locale.nix
       ../../modules/system/input-method.nix
     ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
---
+++ modules/system/boot.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+}
