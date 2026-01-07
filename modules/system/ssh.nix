a3d1e96cd417a410089808008607e47fe381a620
wlog106 <xuanhao8765@gmail.com>
2026-01-07 20:55:57 +0800

    new: add ssh system module-2

---
+++ modules/system/ssh.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  programs.ssh = {
+    extraConfig = "
+      Host debian-in-thinkpad
+        Hostname 140.113.89.250
+        Port 8390
+        User wlog
+        IdentityFile ~/.ssh/id_ed25519
+    ";
+    enableAskPassword = false;
+  };
+}
