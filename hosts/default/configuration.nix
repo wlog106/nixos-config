b410480c733572c3184978e7c6782ca3d13d5698
wlog106 <xuanhao8765@gmail.com>
2026-01-08 08:29:26 +0800

    new: add gnome system module

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -13,28 +13,18 @@
       ../../modules/system/sound.nix
       ../../modules/system/input-method.nix
       ../../modules/system/nvidia.nix
+      ../../modules/system/gnome.nix
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # As of 25.11
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.wayland = true;
-
   services.tailscale.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.wlog = {
     isNormalUser = true;
     description = "wlog";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
 
@@ -54,7 +44,6 @@
     dconf
     dconf2nix
     nautilus
-    gnomeExtensions.blur-my-shell   
   ];
 
   fonts.packages = with pkgs; [
@@ -70,54 +59,5 @@
     siteUrl = "https://mattermost.it.cs.nycu.edu.tw";
   };
 
-  # To disable installing GNOME's suite of applications
-  # and only be left with GNOME shell.
-  services.gnome.core-apps.enable = false;
-  services.gnome.core-developer-tools.enable = false;
-  services.gnome.games.enable = false;
-  environment.gnome.excludePackages = with pkgs; [ 
-    gnome-tour gnome-user-docs gnome-shell-extensions 
-  ];
-
-  services.xserver.excludePackages = with pkgs; [xterm];
-
-
-  programs.dconf.profiles.gdm.databases = [
-    {
-      settings = {
-        "org/gnome/desktop/interface" = {
-          # 1 = 100%, 2 = 200%
-          scaling-factor = lib.gvariant.mkUint32 2;
-        };
-      };
-    }
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11"; 
 }
---
+++ modules/system/gnome.nix
@@ -0,0 +1,37 @@
+{ pkgs, libs, ... }:
+
+{
+
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.wayland = true;
+
+  # To disable installing GNOME's suite of applications
+  # and only be left with GNOME shell.
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.blur-my-shell
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour 
+    gnome-user-docs
+    gnome-shell-extensions
+  ];
+
+  # scale 200% on login screen
+  programs.dconf.profiles.gdm.databases = [
+    {
+      settings = {
+        "org/gnome/desktop/interface" = {
+          # 1 = 100%, 2 = 200%
+          scaling-factor = lib.gvariant.mkUint32 2;
+        };
+      };
+    }
+  ];
+
+}
