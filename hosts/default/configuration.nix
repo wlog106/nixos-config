c89ec482989c2e484578cdbc4db4de0a81faaa26
wlog106 <xuanhao8765@gmail.com>
2026-01-08 08:13:21 +0800

    new: add network system module

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -8,22 +8,13 @@
     [ 
       ./hardware-configuration.nix
       ../../modules/system/boot.nix
+      ../../modules/system/network.nix
       ../../modules/system/timezone-locale.nix
       ../../modules/system/input-method.nix
       ../../modules/system/nvidia.nix
     ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Enable the bluetooth
   hardware.bluetooth.enable = true;
 
